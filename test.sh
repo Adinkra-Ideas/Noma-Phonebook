5d4f8fcb902f589366965368d8c4c91190840705
euyi <euyi@student.42wolfsburg.de>
2024-07-12 22:05:28 +0200

    Refactored Code

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-c++ -std=c++17 -Wextra -Werror -Wall -pedantic -g3 -fsanitize=address main.cpp Contact.cpp PhoneBook.cpp -o noma
+c++ -std=c++17 -Wextra -Werror -Wall -pedantic -g3 -fsanitize=address main.cpp Contact.cpp PhoneBook.cpp main.cpp -o noma
 ./noma
